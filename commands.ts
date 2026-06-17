68141edd3dbd5455468b5d8181a521b5f7d7309b
Mark Dierolf <mdierolf@financialcontent.com>
2026-06-17 00:22:26 -0700

    Reorder chat command imports for consistency

--- commands.ts
+++ commands.ts
@@ -1,7 +1,5 @@
 import chatCompact from "./commands/chat/compact.ts";
 import chatContext from "./commands/chat/context.ts";
-import chatReset from "./commands/chat/reset.ts";
-import chatSend from "./commands/chat/send.ts";
 import modelGet from "./commands/chat/model/get.ts";
 import modelReset from "./commands/chat/model/reset.ts";
 import modelSelect from "./commands/chat/model/select.ts";
@@ -11,6 +9,8 @@ import modelSettingsEnable from "./commands/chat/model/settings/enable.ts";
 import modelSettingsSelect from "./commands/chat/model/settings/select.ts";
 import modelSettingsSet from "./commands/chat/model/settings/set.ts";
 import modelSettingsShow from "./commands/chat/model/settings/show.ts";
+import chatReset from "./commands/chat/reset.ts";
+import chatSend from "./commands/chat/send.ts";
 import toolDisable from "./commands/tool/disable.ts";
 import toolEnable from "./commands/tool/enable.ts";
 import toolHide from "./commands/tool/hide.ts";
